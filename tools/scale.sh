88ed1e0a2b0deebdb2e0c87939a4140d03246e47
Stephan Gatzka <stephan.gatzka@gmail.com>
2016-01-13 20:15:35 +0100

    Add tool to scale images for different resolutions.

---
+++ tools/scale.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+res=(
+  'drawable-xxxhdpi=224'
+  'drawable-xxhdpi=168'
+  'drawable-xhdpi=112'
+  'drawable-hdpi=84'
+  'drawable-mdpi=56'
+)
+
+regex="(.*)=([[:digit:]]+)"
+for element in "${res[@]}"; do
+  [[ $element =~ $regex ]]
+  mkdir -p ${BASH_REMATCH[1]}
+  convert $1 -resize ${BASH_REMATCH[2]}x${BASH_REMATCH[2]} -gravity center -extent ${BASH_REMATCH[2]}x${BASH_REMATCH[2]} -fuzz 5% -transparent white ${BASH_REMATCH[1]}/$2
+done
