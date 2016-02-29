368581cd19dc400a5a224225ce0f16275b7a5eeb
Stephan Gatzka <stephan.gatzka@gmail.com>
2016-02-29 21:55:20 +0100

    Only convert the first image of multi sequence images.

--- tools/scale.sh
+++ tools/scale.sh
@@ -12,6 +12,6 @@ regex="(.*)=([[:digit:]]+)"
 for element in "${res[@]}"; do
   [[ $element =~ $regex ]]
   mkdir -p ${BASH_REMATCH[1]}
-  convert $1 -resize ${BASH_REMATCH[2]}x${BASH_REMATCH[2]} -gravity center -extent ${BASH_REMATCH[2]}x${BASH_REMATCH[2]} -fuzz 5% -transparent white ${BASH_REMATCH[1]}/$2
+  convert $1[0] -resize ${BASH_REMATCH[2]}x${BASH_REMATCH[2]} -gravity center -extent ${BASH_REMATCH[2]}x${BASH_REMATCH[2]} -fuzz 5% -transparent white ${BASH_REMATCH[1]}/$2
   exiftool -overwrite_original -all= ${BASH_REMATCH[1]}/$2
 done
