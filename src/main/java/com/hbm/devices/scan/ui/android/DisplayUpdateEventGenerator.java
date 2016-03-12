c619a493d8b400d48035857f6f7b72cd9f8bb87b
Stephan Gatzka <stephan.gatzka@gmail.com>
2016-03-12 18:38:21 +0100

    Unit test stub to test display update events.

---
+++ src/main/java/com/hbm/devices/scan/ui/android/DisplayUpdateEventGenerator.java
@@ -0,0 +1,94 @@
+/*
+ * Android Scan, an app for scanning and configuring HBM devices.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (C) Stephan Gatzka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.hbm.devices.scan.ui.android;
+
+import com.hbm.devices.scan.announce.Announce;
+
+import java.util.List;
+
+public class DisplayUpdateEventGenerator {
+    private final DisplayNotifier notifier;
+
+    DisplayUpdateEventGenerator(final DisplayNotifier n) {
+        this.notifier = n;
+    }
+
+    void compareLists(final List<Announce> oldList, final List<Announce> newList) {
+        updateRemovals(oldList, newList);
+        updateAdditions(oldList, newList);
+        updateMoves(oldList, newList);
+    }
+
+    private void updateRemovals(final List<Announce> oldList, final List<Announce> newList) {
+        int count = oldList.size();
+        for (int i = count; i >= 0; i--) {
+            final Announce announce = oldList.get(i);
+            if (isInList(newList, announce)) {
+                oldList.remove(i);
+                notifier.notifyRemoveAt(i);
+            }
+        }
+    }
+
+    private void updateAdditions(final List<Announce> oldList, final List<Announce> newList) {
+        for (int i = 0, count = newList.size(); i < count; i++) {
+            final Announce announce = newList.get(i);
+            if (!isInList(oldList, announce)) {
+                oldList.add(announce);
+                notifier.notifyAddAt(i);
+            } else {
+                if (!oldList.contains(announce)) {
+                    oldList.set(i, announce);
+                    notifier.notifyChangeAt(i);
+                }
+            }
+        }
+    }
+
+    private void updateMoves(final List<Announce> oldList, final List<Announce> newList) {
+        for (int toPosition = newList.size() - 1; toPosition >= 0; toPosition--) {
+            final Announce model = newList.get(toPosition);
+            final int fromPosition = oldList.indexOf(model);
+            if (fromPosition >= 0 && fromPosition != toPosition) {
+                final Announce announce = oldList.remove(fromPosition);
+                oldList.add(toPosition, announce);
+                notifier.notifyMoved(fromPosition, toPosition);
+            }
+        }
+    }
+
+    private static boolean isInList(final List<Announce> list, final Announce announce) {
+        for (Announce element : list) {
+            if (element.sameCommunicationPath(announce)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/com/hbm/devices/scan/ui/android/DisplayUpdateEventGeneratorTest.java
@@ -0,0 +1,128 @@
+/*
+ * Android Scan, an app for scanning and configuring HBM devices.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (C) Stephan Gatzka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.hbm.devices.scan.ui.android;
+
+import com.hbm.devices.scan.announce.Announce;
+import com.hbm.devices.scan.announce.AnnounceDeserializer;
+import com.hbm.devices.scan.ui.android.DisplayNotifier;
+
+
+import java.io.IOException;
+import java.lang.Override;
+import java.util.ArrayList;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Properties;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DisplayUpdateEventGeneratorTest  implements DisplayNotifier, Observer {
+
+    private List<Announce> oldList = new ArrayList<Announce>();
+    private List<Announce> newList = new ArrayList<Announce>();
+    private boolean fillNewList = false;
+
+    private static final String DEVICE1;
+    private static final String DEVICE1UPDATE;
+    private static final String DEVICE2;
+    private static final String DEVICE3;
+    private static final String DEVICE4;
+    private static final String DEVICE5;
+    private static final String DEVICE6;
+
+    @Test
+    public void testCompareLists() throws Exception {
+        AnnounceDeserializer parser = new AnnounceDeserializer();
+        parser.addObserver(this);
+        parser.update(null, DEVICE1);
+        parser.update(null, DEVICE2);
+        parser.update(null, DEVICE3);
+        parser.update(null, DEVICE4);
+        parser.update(null, DEVICE5);
+
+        fillNewList = true;
+        parser.update(null, DEVICE1UPDATE);
+        parser.update(null, DEVICE2);
+        parser.update(null, DEVICE6);
+        parser.update(null, DEVICE5);
+        parser.update(null, DEVICE4);
+
+        assertEquals(oldList.size(), 5);
+        assertEquals(newList.size(), 5);
+    }
+
+    @Override
+    public void notifyRemoveAt(int position) {
+
+    }
+
+    @Override
+    public void notifyMoved(int fromPosition, int toPosition) {
+
+    }
+
+    @Override
+    public void notifyChangeAt(int position) {
+
+    }
+
+    @Override
+    public void notifyAddAt(int position) {
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        final Announce announce = (Announce)arg;
+        if (fillNewList) {
+            newList.add(announce);
+        } else {
+            oldList.add(announce);
+        }
+    }
+
+    static {
+        try (final InputStream is = FakeMessageReceiver.class.getResourceAsStream("/devices.properties")) {
+            final Properties props = new Properties();
+            props.load(is);
+            DEVICE1 = props.getProperty("scan.announce.device1");
+            DEVICE1UPDATE = props.getProperty("scan.announce.device1update");
+            DEVICE2 = props.getProperty("scan.announce.device2");
+            DEVICE3 = props.getProperty("scan.announce.device3");
+            DEVICE4 = props.getProperty("scan.announce.device4");
+            DEVICE5 = props.getProperty("scan.announce.device5");
+            DEVICE6 = props.getProperty("scan.announce.device6");
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+}
