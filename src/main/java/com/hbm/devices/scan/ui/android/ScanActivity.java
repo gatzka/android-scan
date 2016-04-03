14a7f9ef52238ecba2a898b296c5071b03a60912
Stephan Gatzka <stephan.gatzka@gmail.com>
2016-04-03 13:10:59 +0200

    Remove unused import.

--- src/main/java/com/hbm/devices/scan/ui/android/ScanActivity.java
+++ src/main/java/com/hbm/devices/scan/ui/android/ScanActivity.java
@@ -34,7 +34,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
