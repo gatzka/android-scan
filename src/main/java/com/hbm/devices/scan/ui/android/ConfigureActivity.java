53012d412c4687fe38adec42800c941ac6787e3b
Stephan Gatzka <stephan.gatzka@gmail.com>
2016-04-04 21:02:07 +0200

    Avoid negated test in if clause.

--- src/main/java/com/hbm/devices/scan/ui/android/ConfigureActivity.java
+++ src/main/java/com/hbm/devices/scan/ui/android/ConfigureActivity.java
@@ -280,15 +280,15 @@ public final class ConfigureActivity extends AppCompatActivity {
                 if (end > start) {
                     String destTxt = dest.toString();
                     String resultingTxt = destTxt.substring(0, dstart) + source.subSequence(start, end) + destTxt.substring(dend);
-                    if (!resultingTxt.matches ("^\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3})?)?)?)?)?)?")) {
-                        return "";
-                    } else {
+                    if (resultingTxt.matches ("^\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3})?)?)?)?)?)?")) {
                         String[] splits = resultingTxt.split("\\.");
                         for (String split : splits) {
                             if (Integer.valueOf(split) > 255) {
                                 return "";
                             }
                         }
+                    } else {
+                        return "";
                     }
                 }
                 return null;
