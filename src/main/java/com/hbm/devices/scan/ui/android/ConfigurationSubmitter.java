f70c116589fa95cc930aae8215ac2b9d1724a6ff
Stephan Gatzka <stephan.gatzka@gmail.com>
2016-04-10 12:24:27 +0200

    Factor out class to submit a new IP Configuration.

---
+++ src/main/java/com/hbm/devices/scan/ui/android/ConfigurationSubmitter.java
@@ -0,0 +1,141 @@
+/*
+ * Android Scan, an app for scanning and configuring HBM devices.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (C) Stephan Gatzka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.hbm.devices.scan.ui.android;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import com.hbm.devices.scan.announce.Announce;
+import com.hbm.devices.scan.configure.ConfigurationDefaultGateway;
+import com.hbm.devices.scan.configure.ConfigurationDevice;
+import com.hbm.devices.scan.configure.ConfigurationInterface;
+import com.hbm.devices.scan.configure.ConfigurationNetSettings;
+import com.hbm.devices.scan.configure.ConfigurationParams;
+import com.hbm.devices.scan.configure.IPv4EntryManual;
+
+class ConfigurationSubmitter implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+    private boolean dhcpEnabled;
+    private final Announce announce;
+    private final ConfigureActivity activity;
+
+    ConfigurationSubmitter(Announce a, ConfigureActivity act) {
+        announce = a;
+        activity = act;
+
+        setEdit(false);
+    }
+
+    @Override
+    public void onClick(View v) {
+        ConfigurationInterface interfaceSettings;
+        final String interfaceName = announce.getParams().getNetSettings().getInterface().getName();
+        if (dhcpEnabled) {
+            interfaceSettings = new ConfigurationInterface(interfaceName, ConfigurationInterface.Method.DHCP);
+        } else {
+            final IPv4EntryManual ipv4Manual = getManualConfiguration();
+            if (ipv4Manual == null) {
+                return;
+            }
+            interfaceSettings = new ConfigurationInterface(interfaceName, ConfigurationInterface.Method.MANUAL, ipv4Manual);
+        }
+        final ConfigurationDevice device = new ConfigurationDevice(announce.getParams().getDevice().getUuid());
+
+        final ConfigurationDefaultGateway gateway = getDefaultGateway();
+        ConfigurationNetSettings netSettings;
+        if (gateway == null) {
+            netSettings = new ConfigurationNetSettings(interfaceSettings);
+        } else {
+            netSettings = new ConfigurationNetSettings(interfaceSettings, gateway);
+        }
+        final ConfigurationParams params = new ConfigurationParams(device, netSettings);
+        activity.sendConfiguration(params);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            dhcpEnabled = true;
+            setEdit(false);
+        } else {
+            dhcpEnabled = false;
+            setEdit(true);
+        }
+    }
+
+    private IPv4EntryManual getManualConfiguration() {
+        final EditText ipv4Address = (EditText) activity.findViewById(R.id.configure_ip_address_edit);
+        String ip;
+        if ((ipv4Address != null) && (ipv4Address.getText() != null) && (ipv4Address.getText().length() > 0)) {
+            ip = ipv4Address.getText().toString();
+        } else {
+            return null;
+        }
+        final EditText ipv4Mask = (EditText) activity.findViewById(R.id.configure_subnet_edit);
+
+        String mask;
+        if ((ipv4Mask != null) && (ipv4Mask.getText() != null) && (ipv4Mask.getText().length() > 0)) {
+            mask = ipv4Mask.getText().toString();
+        } else {
+            return null;
+        }
+        return new IPv4EntryManual(ip, mask);
+    }
+
+    private ConfigurationDefaultGateway getDefaultGateway() {
+        final EditText gateway = (EditText) activity.findViewById(R.id.configure_gateway_ip_edit);
+        if (gateway!= null && gateway.getText() != null && gateway.getText().length() > 0) {
+            return new ConfigurationDefaultGateway(gateway.getText().toString());
+        } else{
+            return null;
+        }
+    }
+
+    private void setEdit(boolean edit) {
+        final EditText ipv4 = (EditText) activity.findViewById(R.id.configure_ip_address_edit);
+        if (ipv4 != null) {
+            ipv4.setEnabled(edit);
+            if (edit) {
+                final InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(ipv4, InputMethodManager.SHOW_IMPLICIT);
+            }
+            final EditText ipv4Mask = (EditText) activity.findViewById(R.id.configure_subnet_edit);
+            if (ipv4Mask != null) {
+                ipv4Mask.setEnabled(edit);
+            }
+            final EditText gateway = (EditText) activity.findViewById(R.id.configure_gateway_ip_edit);
+            if (gateway != null) {
+                gateway.setEnabled(edit);
+            }
+        }
+    }
+}
--- src/main/java/com/hbm/devices/scan/ui/android/ConfigureActivity.java
+++ src/main/java/com/hbm/devices/scan/ui/android/ConfigureActivity.java
@@ -28,7 +28,6 @@
 
 package com.hbm.devices.scan.ui.android;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -36,31 +35,21 @@ import android.support.v7.widget.Toolbar;
 import android.text.InputFilter;
 import android.text.Spanned;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
 import com.hbm.devices.scan.announce.Announce;
 import com.hbm.devices.scan.announce.Device;
-import com.hbm.devices.scan.configure.ConfigurationDefaultGateway;
-import com.hbm.devices.scan.configure.ConfigurationDevice;
-import com.hbm.devices.scan.configure.ConfigurationInterface;
-import com.hbm.devices.scan.configure.ConfigurationNetSettings;
 import com.hbm.devices.scan.configure.ConfigurationParams;
-import com.hbm.devices.scan.configure.IPv4EntryManual;
 
 import java.io.IOException;
 
 public final class ConfigureActivity extends AppCompatActivity {
 
     static final int CONFIGURATION_TIMEOUT = 5000;
-    private Announce announce;
     private ConfigServiceThread configThread;
-    private boolean dhcpEnabled;
 
     private static final InputFilter[] ipAddressFilter;
 
@@ -72,10 +61,8 @@ public final class ConfigureActivity extends AppCompatActivity {
             this.configThread = new ConfigServiceThread();
             configThread.start();
 
-            announce = (Announce) getIntent().getSerializableExtra(DeviceDetailsActivity.DETAILS);
+            final Announce announce = (Announce) getIntent().getSerializableExtra(DeviceDetailsActivity.DETAILS);
             initToolbar(announce);
-
-            setEdit(false);
 
             final EditText ipv4Address = (EditText) findViewById(R.id.configure_ip_address_edit);
             if (ipv4Address != null) {
@@ -89,57 +76,28 @@ public final class ConfigureActivity extends AppCompatActivity {
             if (gateway != null) {
                 gateway.setFilters(ipAddressFilter);
             }
+
+            final ConfigurationSubmitter submitter = new ConfigurationSubmitter(announce, this);
+
             final Switch dhcpSwitch = (Switch) findViewById(R.id.dhcp_switch);
             if (dhcpSwitch != null) {
                 dhcpSwitch.setChecked(true);
-                dhcpSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if (isChecked) {
-                            dhcpEnabled = true;
-                            setEdit(false);
-                        } else {
-                            dhcpEnabled = false;
-                            setEdit(true);
-                        }
-                    }
-                });
+                dhcpSwitch.setOnCheckedChangeListener(submitter);
             }
+
             final Button submit = (Button) findViewById(R.id.submit);
             if (submit != null) {
-                submit.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        ConfigurationInterface interfaceSettings;
-                        final String interfaceName = announce.getParams().getNetSettings().getInterface().getName();
-                        if (dhcpEnabled) {
-                            interfaceSettings = new ConfigurationInterface(interfaceName, ConfigurationInterface.Method.DHCP);
-                        } else {
-                            final IPv4EntryManual ipv4Manual = getManualConfiguration();
-                            if (ipv4Manual == null) {
-                                return;
-                            }
-                            interfaceSettings = new ConfigurationInterface(interfaceName, ConfigurationInterface.Method.MANUAL, ipv4Manual);
-                        }
-                        final ConfigurationDevice device = new ConfigurationDevice(announce.getParams().getDevice().getUuid());
-
-                        final ConfigurationDefaultGateway gateway = getDefaultGateway();
-                        ConfigurationNetSettings netSettings;
-                        if (gateway == null) {
-                            netSettings = new ConfigurationNetSettings(interfaceSettings);
-                        } else {
-                            netSettings = new ConfigurationNetSettings(interfaceSettings, gateway);
-                        }
-                        final ConfigurationParams params = new ConfigurationParams(device, netSettings);
-                        configThread.sendConfiguration(params, ConfigureActivity.this);
-                    }
-                });
+                submit.setOnClickListener(submitter);
             }
         } catch (IOException e) {
             final Toast failureToast = Toast.makeText(this,
                     "Could not start configuration service!", Toast.LENGTH_SHORT);
             failureToast.show();
         }
+    }
+
+    void sendConfiguration(ConfigurationParams params) {
+        configThread.sendConfiguration(params, this);
     }
 
     @Override
@@ -166,53 +124,6 @@ public final class ConfigureActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.activity_back_in, R.anim.activity_back_out);
     }
 
-    private ConfigurationDefaultGateway getDefaultGateway() {
-        final EditText gateway = (EditText) findViewById(R.id.configure_gateway_ip_edit);
-        if (gateway!= null && gateway.getText() != null && gateway.getText().length() > 0) {
-            return new ConfigurationDefaultGateway(gateway.getText().toString());
-        } else{
-            return null;
-        }
-    }
-
-    private IPv4EntryManual getManualConfiguration() {
-        final EditText ipv4Address = (EditText) findViewById(R.id.configure_ip_address_edit);
-        String ip;
-        if ((ipv4Address != null) && (ipv4Address.getText() != null) && (ipv4Address.getText().length() > 0)) {
-            ip = ipv4Address.getText().toString();
-        } else {
-            return null;
-        }
-        final EditText ipv4Mask = (EditText) findViewById(R.id.configure_subnet_edit);
-
-        String mask;
-        if ((ipv4Mask != null) && (ipv4Mask.getText() != null) && (ipv4Mask.getText().length() > 0)) {
-            mask = ipv4Mask.getText().toString();
-        } else {
-            return null;
-        }
-        return new IPv4EntryManual(ip, mask);
-    }
-
-    private void setEdit(boolean edit) {
-        final EditText ipv4 = (EditText) findViewById(R.id.configure_ip_address_edit);
-        if (ipv4 != null) {
-            ipv4.setEnabled(edit);
-            if (edit) {
-                final InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.showSoftInput(ipv4, InputMethodManager.SHOW_IMPLICIT);
-            }
-            final EditText ipv4Mask = (EditText) findViewById(R.id.configure_subnet_edit);
-            if (ipv4Mask != null) {
-                ipv4Mask.setEnabled(edit);
-            }
-            final EditText gateway = (EditText) findViewById(R.id.configure_gateway_ip_edit);
-            if (gateway != null) {
-                gateway.setEnabled(edit);
-            }
-        }
-    }
-
     private void initToolbar(Announce announce) {
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -230,7 +141,7 @@ public final class ConfigureActivity extends AppCompatActivity {
         if (displayName == null || displayName.length() == 0) {
             displayName = device.getUuid();
         }
-        title.append(" ").append(displayName);
+        title.append(' ').append(displayName);
         return title.toString();
     }
 
@@ -241,7 +152,10 @@ public final class ConfigureActivity extends AppCompatActivity {
             public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
                 if (end > start) {
                     String destTxt = dest.toString();
-                    String resultingTxt = destTxt.substring(0, dstart) + source.subSequence(start, end) + destTxt.substring(dend);
+                    String resultingTxt = destTxt.substring(0, dstart) +
+                            source.subSequence(start, end) +
+                            destTxt.substring(dend);
+                    
                     if (resultingTxt.matches ("^\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3})?)?)?)?)?)?")) {
                         String[] splits = resultingTxt.split("\\.");
                         for (String split : splits) {
